c65ac25a64a7441acec1102bb24ee8ab8c748cb5
gidaszewski <gidaszewskifranco@gmail.com>
2024-12-12 12:22:49 +0100

    fix

--- portafolio/data.py
+++ portafolio/data.py
@@ -2,7 +2,9 @@ import json
 
 
 class Media:
-    def __init__(self, github, likedin):
+    def __init__(self, email, cv, github, likedin):
+        self.email = email
+        self.cv = cv
         self.github = github
         self.likedin = likedin
 
